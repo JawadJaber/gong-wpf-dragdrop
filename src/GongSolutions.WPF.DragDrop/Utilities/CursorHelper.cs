ab79de2c894084e4e8979dddb3681c420e569136
JawadJaber <jawadsab@gmail.com>
2022-11-14 08:09:33 +0300

    commit last changes of founders

--- src/GongSolutions.WPF.DragDrop/Utilities/CursorHelper.cs
+++ src/GongSolutions.WPF.DragDrop/Utilities/CursorHelper.cs
@@ -18,33 +18,45 @@ namespace GongSolutions.Wpf.DragDrop.Utilities
 
         public static Point GetCurrentCursorPosition(Visual relativeTo, Point fallBack)
         {
-            var pt = new POINT();
-
-            bool returnValue;
-
             try
             {
-                returnValue = TryGetCursorPos(ref pt);
+                var pt = new POINT();
+
+                bool returnValue;
+
+                try
+                {
+                    returnValue = TryGetCursorPos(ref pt);
+                }
+                catch
+                {
+                    returnValue = false;
+                }
+
+                // Sometimes Win32 will fail this call, such as if you are
+                // not running in the interactive desktop.  For example,
+                // a secure screen saver may be running.
+                if (returnValue == false)
+                {
+                    System.Diagnostics.Debug.WriteLine("GetCursorPos failed!");
+
+                    // pt.x = 0;
+                    // pt.y = 0;
+
+                    return relativeTo.PointFromScreen(fallBack);
+                }
+
+                return relativeTo.PointFromScreen(new Point(pt.x, pt.y));
             }
-            catch
+            catch (System.Exception exx)
             {
-                returnValue = false;
-            }
 
-            // Sometimes Win32 will fail this call, such as if you are
-            // not running in the interactive desktop.  For example,
-            // a secure screen saver may be running.
-            if (returnValue == false)
-            {
-                System.Diagnostics.Debug.WriteLine("GetCursorPos failed!");
-
-                // pt.x = 0;
-                // pt.y = 0;
-
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(exx.Message);
+# endif
                 return relativeTo.PointFromScreen(fallBack);
             }
-
-            return relativeTo.PointFromScreen(new Point(pt.x, pt.y));
+          
         }
     }
 }
